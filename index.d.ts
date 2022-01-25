1ef7c761c23243240022e662a06682bf461f9c40
Eduardo Henrique <eduardohds.dev@outlook.com>
2022-01-25 18:52:43 -0300

    Add num columns and render item

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,38 @@
-import { ReactNode, ReactElement } from "react";
-import { StyleProp, ViewStyle, TextStyle } from "react-native";
+import { ReactNode, ReactElement } from 'react';
+import { StyleProp, ViewStyle, TextStyle } from 'react-native';
 
 type ItemValue = string | number | any | undefined;
 
 export type RNModalSelectItem = {
-  key: string | number;
-  value: ItemValue;
-  label: string;
-  image?: string | ReactNode;
+	key: string | number;
+	value: ItemValue;
+	label: string;
+	image?: string | ReactNode;
 };
 
 export type RNModalSelectProps = {
-  data: Array<RNModalSelectItem>;
-  value: ItemValue;
-  placeholder: string;
-  onSelect: (value: ItemValue) => void;
-  IconRight?: ReactNode;
-  renderComponent?: () => ReactNode;
-  title?: string;
-  cancelText?: string;
-  placeholderTextColor?: string;
-  disabled?: boolean;
-  containerStyle?: StyleProp<ViewStyle>;
-  inputStyle?: StyleProp<ViewStyle>;
-  modalContainerStyle?: StyleProp<ViewStyle>;
-  titleStyle?: StyleProp<TextStyle>;
-  cancelButtonStyle?: StyleProp<ViewStyle>;
-  cancelTextStyle?: StyleProp<TextStyle>;
-  listContainerStyle?: StyleProp<ViewStyle>;
-  itemContainerStyle?: StyleProp<ViewStyle>;
-  itemImageStyle?: StyleProp<ViewStyle>;
-  itemTextStyle?: StyleProp<TextStyle>;
+	data: Array<RNModalSelectItem>;
+	value: ItemValue;
+	placeholder: string;
+	onSelect: (value: ItemValue) => void;
+	IconRight?: ReactNode;
+	renderComponent?: () => ReactNode;
+	title?: string;
+	cancelText?: string;
+	placeholderTextColor?: string;
+	disabled?: boolean;
+	numColumns?: number;
+	renderItem?: ({ item, index }: { item: RNModalSelectItem; index: number }) => ReactNode;
+	containerStyle?: StyleProp<ViewStyle>;
+	inputStyle?: StyleProp<ViewStyle>;
+	modalContainerStyle?: StyleProp<ViewStyle>;
+	titleStyle?: StyleProp<TextStyle>;
+	cancelButtonStyle?: StyleProp<ViewStyle>;
+	cancelTextStyle?: StyleProp<TextStyle>;
+	listContainerStyle?: StyleProp<ViewStyle>;
+	itemContainerStyle?: StyleProp<ViewStyle>;
+	itemImageStyle?: StyleProp<ViewStyle>;
+	itemTextStyle?: StyleProp<TextStyle>;
 };
 
 function RNModalSelect(props: RNModalSelectProps): ReactElement;
